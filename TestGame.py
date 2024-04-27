cf4950a729ffad27ec3742a1d5951585fcbdac65
SirIridium <Luke.a.bushell@gmail.com>
2024-04-26 21:33:35 -0400

    I added a second ball

--- TestGame.py
+++ TestGame.py
@@ -11,7 +11,9 @@ dt = 0
 img = pygame.image.load('TestGameIcon.png')
 pygame.display.set_icon(img)
 
+
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos2 = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -24,8 +26,11 @@ while running:
     screen.fill("purple")
 
     pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "green", player_pos2, 40)
 
     keys = pygame.key.get_pressed()
+
+    # the controls for the red
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
@@ -34,6 +39,18 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    
+    # the controls for the green
+    if keys[pygame.K_UP]:
+        player_pos2.y-= 300 *dt
+    if keys[pygame.K_LEFT]:
+        player_pos2.x-= 300 *dt
+    if keys[pygame.K_DOWN]:
+        player_pos2.y+= 300 *dt
+    if keys[pygame.K_RIGHT]:
+        player_pos2.x+= 300 *dt
+
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -41,6 +58,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(120) / 1000
 
 pygame.quit()
